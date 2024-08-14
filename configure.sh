4997e12406afbab23fe348b48745ea3f959cdd90
sayoridev <mliguori798@gmail.com>
2024-08-14 20:38:21 +0200

    start writing the script

---
+++ configure.sh
@@ -0,0 +1,4 @@
+sudo pacman -S p7zip konsole dolphin okular ark ffmpegthumbs unrar kate unarchiver ntfs-3g zsh power-profiles-daemon
+xclip python-pynvim neovim discord steam spotify-launcher neofetch kdialog catimg chafa xdotool gamemode mangohud obs-studio v4l2loopback-dkms
+lib32-mesa vulkan-intel lib32-vulkan-intel intel-media-driver libva-mesa-driver mesa-vdpau libva-nvidia-driver qt5-quickcontrols2 qt5-quickcontrols
+qt5-graphicaleffects kio-admin dolphin-emu podman distrobox gimp flameshot wine-mono lib32-vkd3d wine-gecko vkd3d ttf-fira-code
