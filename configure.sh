58e03ff8074994222e2050b6cbe87f609818fcfd
sayoridev <mliguori798@gmail.com>
2024-08-15 15:59:23 +0200

    add package

--- configure.sh
+++ configure.sh
@@ -7,6 +7,7 @@ sudo pacman -Syu --noconfirm p7zip konsole dolphin okular ark ffmpegthumbs unrar
 xclip python-pynvim neovim discord steam spotify-launcher neofetch kdialog catimg chafa xdotool gamemode mangohud obs-studio v4l2loopback-dkms
 lib32-mesa vulkan-intel lib32-vulkan-intel intel-media-driver libva-mesa-driver mesa-vdpau libva-nvidia-driver qt5-quickcontrols2 qt5-quickcontrols
 qt5-graphicaleffects kio-admin dolphin-emu podman distrobox gimp flameshot wine-mono lib32-vkd3d wine-gecko vkd3d ttf-fira-code obs-studio onevpl-intel-gpu
+python-pip
 
 sleep 1.5
 wait
